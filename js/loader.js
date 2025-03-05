d95dfc22845c45bb0ac1f130752a2e9420fe8300
eiacc <jtzuya@gmail.com>
2025-03-06 01:33:23 +0800

    update: optimize font

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,9 @@
 export function handleLoader() {
   setTimeout(() => {
     const loader = document.getElementById("loader")
-    loader.setAttribute("data-loading", "false");
+    if (loader) {
+      loader.setAttribute("data-loading", "false");
+    }
 
     document.body.style.overflow = "unset";
   }, 1200);
