5b9e6e7e8b834edd0c8d565202d7b74dd557fd86
eiacc <jtzuya@gmail.com>
2025-03-08 13:11:18 +0800

    update: cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,36 @@
 window.addEventListener("load", async() => {
-  await Loader();
+  Loader();
+  document.addEventListener("LoaderFinished", init) // LoaderFinished is a custom event located at loader.js file
+});
 
+function init() {
   // Header Animations
   const headerAnim            = new Animations('[data-fade-down]', 'header', 500);
   const headerAnimAsDeps      = headerAnim.stagger();
 
+  // Banner
   const bannerSVGAnim         = new Animations('[data-fade-down]', '#banner', 700, 35);
   const bannerTextAnim        = new Animations('[data-fade]', '#banner', 375, 100);
   const bannerSVGAnimAsDeps   = bannerSVGAnim.stagger();
   const bannerTextAnimAsDeps  = bannerTextAnim.opacity();
 
-  const observe = new ObserveLoader({ preloader: '[data-preloader]', first_visible_section: '#banner' });
-  observe.init([headerAnimAsDeps, bannerSVGAnimAsDeps, bannerTextAnimAsDeps]);
+  // Observe Loading Screen
+  const observeLoader         = new ObserveLoader({ preloader: '[data-preloader]', first_visible_section: '#banner' });
+  observeLoader.init([headerAnimAsDeps, bannerSVGAnimAsDeps, bannerTextAnimAsDeps]);
 
+  // Link Scroll To Section
   const scrollIntoView = new ViewSectionScroll('[data-scroll-to]')
   scrollIntoView.init()
 
+  // Parallax
+  const parallaxImagesAnim        = new Animations('[data-fade]', '#parallaxContainer', 175, 50)
+  const parallaxImagesAnimAsDeps  = parallaxImagesAnim.opacity()
+
+  // Parallax Sticky Section
+  const stickySection             = new StickySection('[data-sticky-section]', '[data-transform-container]', '[data-dot]')
+  stickySection.init(parallaxImagesAnimAsDeps);
+
+  // Smooth Scroll and Custom Cursor
   if (window.innerWidth > 1024) {
     const cursor = new CustomCursor('cursor')
     cursor.hover('[data-cursor-size]')
@@ -28,10 +43,4 @@ window.addEventListener("load", async() => {
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Custom easing function (optional)
     });
   }
-
-  const parallaxImagesAnim        = new Animations('[data-fade]', '#parallaxContainer', 175, 50)
-  const parallaxImagesAnimAsDeps  = parallaxImagesAnim.opacity()
-
-  const stickySection             = new StickySection('[data-sticky-section]', '[data-transform-container]', '[data-dot]')
-  stickySection.init(parallaxImagesAnimAsDeps);
-});
+}
