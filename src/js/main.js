1dcf9f1a0ece4076480692aa42f20dda00b68962
eiacc <jtzuya@gmail.com>
2025-03-08 10:50:44 +0800

    updates: update: use load instead of DOMContentLoaded for the preloader animations to work seamlessly update: usage of animation instances as dependency injection update: instance of observeloader and initialize dependency array update: condition for lenis to only use on larger screens

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,33 @@
-window.addEventListener("DOMContentLoaded", async() => {
-  await handleLoader();
-  observeBanner();
+window.addEventListener("load", async() => {
+  await Loader();
 
-  const lenis = new Lenis({
-    autoRaf: true,
-    smooth: true, // Enables smooth scrolling
-    lerp: 0.1, // Lower = smoother, Higher = snappier
-    duration: 1, // Adjusts scroll ease duration
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Custom easing function (optional)
-  });
+  // Header Animations
+  const headerAnim            = new Animations('[data-fade-down]', 'header', 500);
+  const headerAnimAsDeps      = headerAnim.stagger();
+
+  const bannerSVGAnim         = new Animations('[data-fade-down]', '#banner', 700, 35);
+  const bannerTextAnim        = new Animations('[data-fade]', '#banner', 375, 100);
+  const bannerSVGAnimAsDeps   = bannerSVGAnim.stagger();
+  const bannerTextAnimAsDeps  = bannerTextAnim.opacity();
+
+  const observe = new ObserveLoader({ preloader: '[data-preloader]', first_visible_section: '#banner' });
+  observe.init([headerAnimAsDeps, bannerSVGAnimAsDeps, bannerTextAnimAsDeps]);
 
   const scrollIntoView = new ViewSectionScroll('[data-scroll-to]')
   scrollIntoView.init()
 
-  const cursor = new CustomCursor('cursor')
-  cursor.hover('[data-cursor-size]')
+  if (window.innerWidth > 1024) {
+    const cursor = new CustomCursor('cursor')
+    cursor.hover('[data-cursor-size]')
+
+    const lenis = new Lenis({
+      autoRaf: true,
+      smooth: true,   // Enables smooth scrolling
+      lerp: 0.1,      // Lower = smoother, Higher = snappier
+      duration: 1,    // Adjusts scroll ease duration
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Custom easing function (optional)
+    });
+  }
 
   const stickySection = new StickySection('[data-sticky-section]', '[data-transform-container]', '[data-dot]')
   stickySection.init();
